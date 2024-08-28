79df746aa6e269c6f05825e16b65eb8f78fa1f7f
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-08-28 18:10:04 -0300

    Eslint
    Estilos configurados

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
-import { env } from "./env";
-import { app } from "./app";
+import { env } from './env'
+import { app } from './app'
 
-app.listen({
+app
+  .listen({
     port: env.PORT,
     host: '0.0.0.0',
-}).then(() => {
-    console.log('Running on port 3333! 🚀');
-});
+  })
+  .then(() => {
+    console.log('Running on port 3333! 🚀')
+  })
