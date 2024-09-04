93894ded23b3c4b0d1e6571f8ed79964f2e72171
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-09-04 18:01:55 -0300

    Primeira Rota

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,25 @@
 import fastify from 'fastify'
-import { PrismaClient } from '@prisma/client'
+import { z } from 'zod'
+import { prisma } from './lib/prisma'
 
 export const app = fastify()
 
-const prisma = new PrismaClient()
+app.post('/users', async (request, reply) => {
+  const registerUserSchema = z.object({
+    name: z.string().min(3),
+    email: z.string().email(),
+    password: z.string().min(6),
+  })
 
-prisma.user.create({
-  data: {
-    name: 'Alice',
-    email: 'Alice@gmail.com',
-  },
+  const { name, email, password } = registerUserSchema.parse(request.body)
+
+  await prisma.user.create({
+    data: {
+      name,
+      email,
+      password_hash: password,
+    },
+  })
+
+  return reply.status(201).send()
 })
