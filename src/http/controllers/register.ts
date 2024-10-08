dffe9b97beeca88bea74b2943f86324e13231303
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-10-08 17:48:37 -0300

    Factory Pattern nos Controller
    Authenticate e Register

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,8 +1,7 @@
 import { FastifyRequest, FastifyReply } from 'fastify'
 import { z } from 'zod'
-import { RegisterUseCase } from '@/use-cases/register'
-import { PrismaUsersRepository } from '@/repositories/prisma/prisma-users-repository'
 import { UserAlreadyExistsError } from '@/use-cases/errors/user-already-exists-erros'
+import { makeRegisteruseCase } from '@/use-cases/factories/make-register-use-case'
 
 export async function register(
   request: FastifyRequest,
@@ -17,8 +16,7 @@ export async function register(
   const { name, email, password } = registerUserSchema.parse(request.body)
 
   try {
-    const usersRepository = new PrismaUsersRepository()
-    const registerUseCase = new RegisterUseCase(usersRepository)
+    const registerUseCase = makeRegisteruseCase()
 
     await registerUseCase.execute({ name, email, password })
   } catch (err) {
