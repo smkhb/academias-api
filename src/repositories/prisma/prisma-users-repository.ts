ba5c520cefdd33dbe00f04195261ea0de3491662
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-10-28 17:24:48 -0300

    criando/arrumando repo do prisma

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -3,13 +3,22 @@ import { Prisma } from '@prisma/client'
 import { UsersRepository } from '../users-repository'
 
 export class PrismaUsersRepository implements UsersRepository {
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    return user
+  }
+
   async findByEmail(email: string) {
     const user = await prisma.user.findUnique({
       where: {
         email,
       },
     })
-    return use
+    return user
   }
 
   async create(data: Prisma.UserCreateInput) {
