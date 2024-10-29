c892a80aa78df1a7bd0bd9c63ec2fdf455912faa
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-10-29 16:59:19 -0300

    DB isolado para testes e2e

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,14 +1,41 @@
+import { PrismaClient } from '@prisma/client'
+import 'dotenv/config'
+
+import { execSync } from 'node:child_process'
+import { randomUUID } from 'node:crypto'
 import { Environment } from 'vitest'
+
+const prisma = new PrismaClient()
+
+function generateDataBaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL is not defined')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
 
 export default <Environment>(<unknown>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const dataBaseURL = generateDataBaseURL(schema)
+
+    process.env.DATABASE_URL = dataBaseURL
+
+    execSync('npx prisma migrate deploy --preview-feature', {})
 
     return {
       async teardown() {
-        console.log('Teardown')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`,
+        )
+        await prisma.$disconnect()
       },
     }
   },
