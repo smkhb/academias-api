ac86418914ebeb9cec907355e35dd60ef26026ff
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-11-21 14:39:28 -0300

    body -> query

--- src/http/controllers/gyms/search.ts
+++ src/http/controllers/gyms/search.ts
@@ -11,7 +11,7 @@ export async function search(
     page: z.coerce.number().min(1).default(1),
   })
 
-  const { q, page } = searchGymsQuerySchema.parse(request.body)
+  const { q, page } = searchGymsQuerySchema.parse(request.query)
 
   const searchGymsUseCase = makeSearchGymsUseCase()
 
