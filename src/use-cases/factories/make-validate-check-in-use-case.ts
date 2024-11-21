235461b72d0b871073c802e99fff4efca21bd8c2
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-11-21 12:41:12 -0300

    Erro grande em factories

--- src/use-cases/factories/make-validate-check-in-use-case.ts
+++ src/use-cases/factories/make-validate-check-in-use-case.ts
@@ -1,9 +1,9 @@
 import { PrismaCheckInsRepository } from '@/repositories/prisma/prisma-check-ins-repository'
-import { FetchUserCheckInsHistoryUseCase } from '../fetch-user-check-ins-history'
+import { ValidateCheckInUseCase } from '../validate-check-in'
 
 export function makeValidateCheckInUseCase() {
   const checkInsRepository = new PrismaCheckInsRepository()
-  const useCase = new FetchUserCheckInsHistoryUseCase(checkInsRepository)
+  const useCase = new ValidateCheckInUseCase(checkInsRepository)
 
   return useCase
 }
