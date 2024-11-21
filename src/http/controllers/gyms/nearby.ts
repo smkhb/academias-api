94b6b1c08fe0efbf4b4ebe828fedc1e19d9d99a8
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-11-21 12:41:24 -0300

    body -> query

--- src/http/controllers/gyms/nearby.ts
+++ src/http/controllers/gyms/nearby.ts
@@ -11,7 +11,7 @@ export async function nearby(
     longitude: z.number().refine((value) => Math.abs(value) <= 180),
   })
 
-  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.body)
+  const { latitude, longitude } = nearbyGymsQuerySchema.parse(request.query)
 
   const featchNearbyGymsUseCase = makeFetchNearbyGymsUseCase()
 
