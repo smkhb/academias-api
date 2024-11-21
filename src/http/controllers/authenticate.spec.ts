5d7b0d1623473252b33fb3ca3794ed8ad63c6cc3
Samuel Bernardo <samuel.hagers.bernardo@gmail.com>
2024-11-21 10:20:30 -0300

    e2e autenticação

--- src/http/controllers/authenticate.spec.ts
+++ src/http/controllers/authenticate.spec.ts
@@ -1,3 +1,29 @@
-import { test } from 'vitest'
+import request from 'supertest'
+import { app } from '@/app'
+import { afterAll, beforeAll, describe, expect, it } from 'vitest'
 
-test('ok', () => {})
+describe('Authenticate (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+
+  it('should be able to authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'John Doe',
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    })
+
+    const response = await request(app.server).post('/sessions').send({
+      email: 'johndoe@exemple.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toBe(200)
+    expect(response.body).toEqual({ token: expect.any(String) })
+  })
+})
